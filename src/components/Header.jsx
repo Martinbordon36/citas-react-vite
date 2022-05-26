3ffcebb36519526e37ad1eca7e86cdf2b63b9cf2
Martin Bordon <martinbordon36@gmail.com>
2022-05-25 21:14:24 -0300

    Actualizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header (){
 
     return(
    
-        <h1 className="font-black text-5xl text-center w-2/3 mx-auto">Seguimiento Pacientes {''}
+        <h1 className="font-black text-5xl text-center w-2/3 mx-auto">Seguimiento Pacientes GIT {''}
             <span className="text-indigo-700">Veterinaria </span>
         </h1>
     
